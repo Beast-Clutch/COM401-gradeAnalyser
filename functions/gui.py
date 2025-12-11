652219620473cdd9fd39799bfd2fcb6f280b4006
Jack <67729936+Beast-Clutch@users.noreply.github.com>
2025-12-11 14:08:05 +0000

    Removed Unnessacery container

--- functions/gui.py
+++ functions/gui.py
@@ -8,14 +8,10 @@ def startGUI():
     #Window Title
     root.title("Student Grade Analyser")
     root.geometry("1280x720")
-    mainframe = ttk.Frame(root, padding=4)
-    mainframe.grid(column=0, row=0, sticky="nsew")
     root.rowconfigure(0, weight=1)
     root.columnconfigure(0, weight=1)
-    mainframe.rowconfigure(0, weight=1)
-    mainframe.columnconfigure(0, weight=1)
-
-    notebook = ttk.Notebook(mainframe)
+    #Notebook Tabs - Allows for different pages accessible through a menu at the top.
+    notebook = ttk.Notebook(root)
     notebook.grid(row=0, column=0, sticky="nsew")
     f1 = ttk.Frame(notebook)
     f2 = ttk.Frame(notebook)
