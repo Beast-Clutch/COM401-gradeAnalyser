bd23e90181bb4f010d785dd9803d2102307796b2
Jack <67729936+Beast-Clutch@users.noreply.github.com>
2025-12-11 13:44:30 +0000

    Started Work on the GUI after confirming the CSV and JSON Import works. Tabs added for different section of program such as importing data, seeing data in db and analysis.

--- functions/gui.py
+++ functions/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 
 # Opening GUI Window with tkinter
@@ -6,5 +7,30 @@ def startGUI():
     root = tk.Tk()
     #Window Title
     root.title("Student Grade Analyser")
+    root.geometry("1280x720")
+    mainframe = ttk.Frame(root, padding=4)
+    mainframe.grid(column=0, row=0, sticky="nsew")
+    root.rowconfigure(0, weight=1)
+    root.columnconfigure(0, weight=1)
+    mainframe.rowconfigure(0, weight=1)
+    mainframe.columnconfigure(0, weight=1)
+
+    notebook = ttk.Notebook(mainframe)
+    notebook.grid(row=0, column=0, sticky="nsew")
+    f1 = ttk.Frame(notebook)
+    f2 = ttk.Frame(notebook)
+    f3 = ttk.Frame(notebook)
+    notebook.add(f1, text="Grade Input")
+    notebook.add(f2, text="Grade Database")
+    notebook.add(f3, text="Grade Analysis")
+
+
+    ttk.Label(f1, text="Tab 1: Grade Input").pack(anchor="w")
+    ttk.Label(f2, text="Tab 2: Grade Database").pack(anchor="w")
+    ttk.Label(f3, text="Tab 3: Grade Analysis").pack(anchor="w")
+
+
+
+
     root.mainloop()
 
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from functions.gui import startGUI
 import functions.fileIO
 
 def main():
-    #\Initialisation of GUI window
-    #startGUI()
-
+    #Initialisation of GUI window
+    startGUI()
+    print("Main Application Starting")
     ##Testing
-    test = CSVImport(r"C:\Users\jackt\OneDrive - University of Chichester\COM401-ProceduralPrograming\gradeAnalyser\Notes\student_grades.csv")
-    test = JSONImport(r"C:\Users\jackt\OneDrive - University of Chichester\COM401-ProceduralPrograming\gradeAnalyser\Notes\student_grades.json")
+    #test = CSVImport(r"C:\Users\jackt\OneDrive - University of Chichester\COM401-ProceduralPrograming\gradeAnalyser\Notes\student_grades.csv")
+    #print(test)
+    #test = JSONImport(r"C:\Users\jackt\OneDrive - University of Chichester\COM401-ProceduralPrograming\gradeAnalyser\Notes\student_grades.json")
+    #print(test)
 # Main Function Starter
 if __name__ == "__main__":
     main()
